fdea0474e797b88faeed7abb4f7e1c4e78c12696
Arthur Patriota <arthurpatriota21@gmail.com>
2025-01-16 14:00:16 -0400

    no sound

--- js/script.js
+++ js/script.js
@@ -4,9 +4,6 @@ const pipe = document.querySelector('.pipe')
 const start = document.querySelector('.start')
 const gameOver = document.querySelector('.game-over')
 
-audioStart = new Audio('./src/audio/audio_theme.mp3')
-audioGameOver = new Audio('./src/audio/audio_gameover.mp3')
-
 
 const startGame = () => {
     pipe.classList.add('pipe-animation')
